537225c33298aa773973319e19ef72f8e3eb3315
maxonclaxon <satamaks@gmail.com>
2022-11-03 13:50:51 +0300

    feat: add superuser creation procedure (now superuser will be created at startup in dev)

--- app/constants.py
+++ app/constants.py
@@ -5,3 +5,8 @@ ADMIN_GROUP_NAME = "admin"                          # group name for is_admin ch
 DEFAULT_USER_GROUP_NAME = "user"                    # default user group name (sets this group for new users)
 MAX_STORIES_COUNT = 10                              # max returned stories count
 MAX_ARTICLES_COUNT = 10                             # max returned stories count
+DEV_SUPERUSER_LOGIN = "admin@mail.ru"               # superuser login for development (user will be created if 'development' set in ENVRIONMENT env variable)
+# superuser password for development.
+# ATTENTION! PASSWORD SHOULD BE HASHED WITH CryptContext(schemes=["bcrypt"]).hash(PWD). Only then password can be pasted
+# here. Also, you need change user password in postman (in postman password should not be crypted)
+DEV_SUPERUSER_PASSWORD = "$2b$12$W6aPE5HGb8dcP1leBZhICOGw8kq095MzAffo5jCBM10YP1BPXVBZa"
---
+++ app/utils/utility.py
@@ -0,0 +1,35 @@
+import sqlalchemy
+
+from app.api.routes.v1.groups.utils import get_group_model_or_create_if_not_exists
+from app.constants import ADMIN_GROUP_NAME, DEV_SUPERUSER_LOGIN, DEV_SUPERUSER_PASSWORD
+from app.database.models.base import Users
+from app.database.manager import DatabaseManagerAsync
+
+from app.log import Loggers
+
+async def create_superuser() -> None:
+    """
+    Method creates superuser (admin) for development.
+
+    :return: None
+    """
+    logger = Loggers.get_default_logger()
+    logger.info("Creating superuser")
+    async with DatabaseManagerAsync.get_instance().get_session() as session:
+        user_is_already_created = (
+            await session.execute(sqlalchemy.select(Users).where(Users.username==DEV_SUPERUSER_LOGIN))
+        ).scalars().first()
+        if user_is_already_created:
+            logger.info("Superuser already created")
+            return
+        new_user = Users(
+            username=DEV_SUPERUSER_LOGIN,
+            password=DEV_SUPERUSER_PASSWORD,
+            email=DEV_SUPERUSER_LOGIN,
+            name=DEV_SUPERUSER_LOGIN,
+            info=DEV_SUPERUSER_LOGIN,
+        )
+        new_user.groups.append(await get_group_model_or_create_if_not_exists(group_name=ADMIN_GROUP_NAME,
+                                                                             session=session))
+        session.add(new_user)
+        logger.info("Superuser created")
