d3365957ba247525073e3d866af0cf066401a155
maxonclaxon <satamaks@gmail.com>
2024-03-20 01:48:13 +0300

    feat: update logging config

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ from uvicorn.config import LOGGING_CONFIG
 
 from app.api.app import app
 
-
+LOGGING_CONFIG["formatters"]["default"]["fmt"] = "%(asctime)s [%(name)s] %(levelprefix)s %(message)s"
 
 if __name__ == "__main__":
-    LOGGING_CONFIG["formatters"]["default"]["fmt"] = "%(asctime)s [%(name)s] %(levelprefix)s %(message)s"
     uvicorn.run(app, host="0.0.0.0", port=8000)
